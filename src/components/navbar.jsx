24076d5e28d5a7d38e2f42e9d8ba84d3410ad1ff
Aniket Raj <araj52444@gmail.com>
2026-01-31 10:38:25 +0530

    add theme provider and toggle functionality; integrate @clerk/themes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,17 @@
 import { NavLink } from "react-router";
 import { Button } from "@/components/ui/button";
 import { UserButton, useAuth } from "@clerk/clerk-react";
+import { useTheme } from "@/components/theme-provider";
+import { Sun, Moon } from "lucide-react";
 
 const Navbar = () => {
   const { isLoaded, isSignedIn } = useAuth();
+
+  const { theme, setTheme } = useTheme();
+
+  function toggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
 
   return (
     <nav className="sticky top-0 z-100 flex h-14 shadow-sm items-center justify-between px-10">
@@ -25,7 +33,22 @@ const Navbar = () => {
         <UserButton
           userProfileMode="navigation"
           userProfileUrl="/auth/profile"
-        />
+          showName
+        >
+          <UserButton.MenuItems>
+            <UserButton.Action
+              label={theme === "light" ? "Dark Mode" : "Light Mode"}
+              labelIcon={
+                theme === "light" ? (
+                  <Moon className="size-4" />
+                ) : (
+                  <Sun className="size-4" />
+                )
+              }
+              onClick={toggleTheme}
+            />
+          </UserButton.MenuItems>
+        </UserButton>
       )}
 
       {isLoaded && !isSignedIn && (
--- src/layout/root-layout.jsx
+++ src/layout/root-layout.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/navbar";
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 import { ClerkProvider } from "@clerk/react-router";
+
+import { shadcn } from "@clerk/themes";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -13,14 +17,21 @@ if (!PUBLISHABLE_KEY) {
 
 const RootLayout = () => {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <div className="flex flex-col h-screen w-full">
-        <Navbar />
-        <div className="flex-1 overflow-auto">
-          <Outlet />
+    <ThemeProvider defaultTheme="dark" storageKey="imagine-ui-theme">
+      <ClerkProvider 
+        publishableKey={PUBLISHABLE_KEY} 
+        appearance={{
+          theme : shadcn
+        }}
+      >
+        <div className="flex flex-col h-screen w-full">
+          <Navbar />
+          <div className="flex-1 overflow-auto">
+            <Outlet />
+          </div>
         </div>
-      </div>
-    </ClerkProvider>
+      </ClerkProvider>
+    </ThemeProvider>
   );
 };
 
